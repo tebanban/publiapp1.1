9e7590e87799e5d4a349440fe3a0b165b9bc30af
tebanban <estebancalf@gmail.com>
2022-04-23 23:49:34 +0000

    flux updated

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,16 +4,19 @@ import logoCover from "../../img/logo-publiex.png";
 import "../../styles/home.scss";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	console.log(process.env.VARIABLE_NAME);
+  console.log(process.env.VARIABLE_NAME);
+  console.log(store.siteValla);
 
-	return (
-		<div className="text-center mt-5">
-			<p className="m-5">
-				<img src={logoCover} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <p className="m-5">
+        <img src={logoCover} />
+      </p>
+      <div className="alert alert-info">
+        {store.message || "Loading message from the backend..."}
+      </div>
+    </div>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,53 +1,39 @@
 import Tempdata from "../../js/store/tempdata.json"; //import fake database
 
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
+  return {
+    store: {
+      message: null,
 
-			site: Tempdata //json object imported
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "orange");
-			},
+      site: Tempdata, //json object imported
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      siteValla: [],
+    },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+    actions: {
+      getVallas: () => {
+        //fetching vallas table
+        fetch(process.env.BACKEND_URL + "/api/valla")
+          .then((res) => res.json())
+          .then((data) => {
+            setStore({ siteValla: data }), console.log(data);
+          })
+          .catch((error) => console.log("Error get vallas", error));
+      },
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+      // Use getActions to call a function within a fuction
+
+      getMessage: () => {
+        // fetching data from the backend
+        fetch(process.env.BACKEND_URL + "/api/hello")
+          .then((resp) => resp.json())
+          .then((data) => setStore({ message: data.message }))
+          .catch((error) =>
+            console.log("Error loading message from backend", error)
+          );
+      },
+    },
+  };
 };
 
 export default getState;
