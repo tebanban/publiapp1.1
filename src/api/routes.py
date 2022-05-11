1bfe1709b465e5462b608dcedff3e43cbcd7ea3a
estebanense <estcespedes@yahoo.com>
2022-05-11 23:54:22 +0000

    routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -44,7 +44,10 @@ def get_single_user(id):
 
     if request.method == "PUT":
         user= User.query.get(id)
+        user.name= request.json["name"]
         user.email= request.json["email"]
+        user.is_active= request.json["is_active"]
+        user.created= request.json["created"]
         user.role_id= request.json["role_id"]
         db.session.commit()
         return jsonify(user.serialize()), 200
@@ -69,8 +72,16 @@ def get_single_valla(id):
     
     if request.method == 'PUT':   
         valla = Valla.query.get(id)
-        valla.name = request.json['name']  
         valla.code = request.json['code'] 
+        valla.name = request.json['name']  
+        valla.format = request.json['format'] 
+        valla.ligth = request.json['ligth']
+        valla.price_low = request.json['price_low']
+        valla.price_high = request.json['price_high']
+        valla.view = request.json['view']
+        valla.route = request.json['route']
+        valla.created = request.json['created']
+        valla.comment = request.json['comment']
         valla.owner_id = request.json['owner_id']   
         db.session.commit()
         return jsonify(valla.serialize()), 200
@@ -96,10 +107,43 @@ def get_single_owner(id):
     if request.method == 'PUT':   
         owner = Owner.query.get(id)
         owner.name = request.json['owner_name']  
-        owner.code = request.json['owner_code']  
+        owner.code = request.json['owner_code'] 
+        owner.created = request.json['created']
+        owner.phone = request.json['owner_phone']
+        owner.email = request.json['owner_email'] 
+        owner.company = request.json['owner_company']
         db.session.commit()
         return jsonify(owner.serialize()), 200
 
+        
+# Get all clients:
+
+@api.route("/client/", methods=["GET"])   
+def get_all_clients():
+
+    all_clients = Owner.query.all()
+    all_clients = list(map(lambda x: x.serialize(), all_clients)) 
+    return jsonify(all_clients), 200
+
+# Handle single owner:
+
+@api.route("/client/<int:id>", methods=["GET", "PUT"])  
+def get_single_client(id):
+
+    if request.method == 'GET':                                           
+        single_client = Client.query.get(id)
+        return jsonify(single_client.serialize()), 200
+    
+    if request.method == 'PUT':   
+        client = Owner.query.get(id)
+        client.name = request.json['client_name']  
+        client.code = request.json['client_code'] 
+        client.created = request.json['created']
+        client.phone = request.json['client_phone']
+        client.email = request.json['client_email'] 
+        client.company = request.json['client_company']
+        db.session.commit()
+        return jsonify(client.serialize()), 200
 
 
 
