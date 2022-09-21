36d6697c4b9613551fb1d13320226ae289d82ad5
tebanban <estebancalf@gmail.com>
2022-09-21 21:51:48 +0000

    Update form vallas

--- src/front/js/component/form_valla.js
+++ src/front/js/component/form_valla.js
@@ -9,56 +9,98 @@ export const FormValla = () => {
   const params = useParams();
 
   return (
-    <div className="form">
-      <h3>Registrar nueva valla</h3>
-      <Form.Group>
-        <Form.Label htmlFor="inputAddress">Código</Form.Label>
-        <input type="text" className="form-control" placeholder="SJ65"></input>
+    <Form className="form">
+      <h4>Registrar nueva valla</h4>
+
+      <Form.Group className="form-group my-2">
+        <label for="code" className="col-md-2 control-label">
+          Código
+        </label>
+        <div className="col-md-10">
+          <input
+            className="form-control"
+            id="code"
+            maxlength="10"
+            name="code"
+            required=""
+            type="text"
+          />
+        </div>
       </Form.Group>
-      <Form.Group>
-        <label htmlFor="inputAddress">Nombre</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Nombre"
-        ></input>
+      <Form.Group className="form-group my-2">
+        <label for="name" className="col-md-2 control-label">
+          Nombre
+        </label>
+        <div className="col-md-10">
+          <input
+            className="form-control"
+            id="code"
+            maxlength="10"
+            name="name"
+            required=""
+            type="text"
+          />
+        </div>
       </Form.Group>
-      <Form.Group>
-        <label htmlFor="inputAddress">Sentido</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="San José - Alajuela"
-        ></input>
+      <Form.Group className="form-group my-2">
+        <label for="format" className="col-md-2 control-label">
+          Format
+        </label>
+        <div className="col-md-10">
+          <select className="form-control" id="format" name="format">
+            <option value="" selected disabled hidden>
+              Seleccionar...
+            </option>
+            <option value="horizontal">Horizontal</option>
+            <option value="vertical">Vertical</option>
+          </select>
+        </div>
       </Form.Group>
-      <Form.Group>
-        <label htmlFor="inputAddress">Ruta</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="General Cañas"
-        ></input>
+      <Form.Group className="form-group my-2">
+        <label for="view" className="col-md-2 control-label">
+          Sentido
+        </label>
+        <div className="col-md-10">
+          <input
+            className="form-control"
+            id="view"
+            maxlength="20"
+            name="view"
+            required=""
+            type="text"
+          />
+        </div>
       </Form.Group>
-      <Form.Group>
-        <label htmlFor="inputAddress">Usuario</label>
-        <input type="" className="form-control"></input>
+      <Form.Group className="form-group my-2">
+        <label for="route" className="col-md-2 control-label">
+          Ruta
+        </label>
+        <div className="col-md-10">
+          <input
+            className="form-control"
+            id="route"
+            maxlength="20"
+            name="route"
+            required=""
+            type="text"
+          />
+        </div>
       </Form.Group>
-      <Form.Group>
-        <label htmlFor="inputAddress">Tipo</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Unipolar"
-        ></input>
+      <Form.Group className="form-group my-2">
+        <label for="code" className="col-md-2 control-label">
+          User
+        </label>
+        <div className="col-md-10">
+          <select className="form-control" id="user" name="user">
+            <option value="" selected disabled hidden>
+              Seleccionar...
+            </option>
+            <option value="user1">User1</option>
+            <option value="user2">User2</option>
+            <option value="user3">User3</option>
+          </select>
+        </div>
       </Form.Group>
-      <Form.Group>
-        <label htmlFor="inputAddress">Propietario</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Propietario"
-        ></input>
-      </Form.Group>
-    </div>
+    </Form>
   );
 };
