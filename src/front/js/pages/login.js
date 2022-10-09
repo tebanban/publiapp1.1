363d2083f4caa8372fc0dd7e8ea01aff9ebe2b28
tebanban <estebancalf@gmail.com>
2022-10-09 04:46:22 +0000

    Create login page

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,39 @@
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
+import { Form, Button } from "react-bootstrap";
+import "../../styles/home.scss";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const sendCredentials = () => {
+    actions.sendCredentials(email, password);
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <Form className="form">
+        <Form.Group>
+          <h1>Login Page</h1>
+          <input
+            type="text"
+            placeholder="email"
+            value="email"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="password"
+            value="password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <Button variant="primary" onClick={sendCredentials}>
+            Enviar
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
