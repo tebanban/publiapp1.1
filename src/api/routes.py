644bbf1f1f74328e7c18e78b858a7cf44b3aaba9
tebanban <estebancalf@gmail.com>
2022-10-09 00:38:01 +0000

    Add JWT variables , routes, and app.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,23 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Valla, Client, Owner, Order, Payment
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/token", methods=["POST"])
+def get_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
