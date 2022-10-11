a76729f355111b805054000753d9d43ab9afcaae
estebanense <estcespedes@yahoo.com>
2022-10-11 20:16:03 +0000

    Add logout dropdown button

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,21 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import logoNav from "../../img/logo-nav.png";
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
   const token = sessionStorage.getItem("token");
-  const current_user = store.current_user
+  const current_user = store.current_user;
+  
 
-  const logout = ()=>{
+  const logout = () => {
     actions.logout();
-  }
+  };
 
   return (
-    <nav className="navbar navbar-light mb-3 ">
+    <Nav className="navbar navbar-light mb-3 ">
       <Link to="/">
         <img
           className="navbar-brand px-2 mb-0 h1"
@@ -23,23 +26,26 @@ export const Navbar = () => {
         />
       </Link>
       <div className="ml-auto">
-        {token && token != "" && token != "undefined" ? (
-          <button className="btn btn-primary mx-2" onClick={logout}>
-            <i className="fa fa-user" aria-hidden="true"></i>{" " + current_user}
-          </button>
-        ) : (
-          <Link to="/login">
-            <button className="btn btn-primary mx-2">Login</button>
-          </Link>
-        )}
-
         <Link to="/app">
           <button className="btn btn-primary mx-2">App</button>
         </Link>
         <Link to="/">
           <button className="btn btn-primary mx-2">Home</button>
         </Link>
+        {token && token != "" && token != "undefined" ? (
+          <NavDropdown
+            title= {current_user}
+            id="basic-nav-dropdown"
+            className="d-inline-block btn  "
+          >
+            <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <Link to="/login">
+            <button className="btn btn-primary mx-2">Login</button>
+          </Link>
+        )}
       </div>
-    </nav>
+    </Nav>
   );
 };
